35195e52e9aae0210164485c83e31508270cd973
Jared Volpe <plainspace@gmail.com>
2016-02-25 00:08:41 -0800

    Create the custom tab bar

--- Tumblr/TabBarViewController.swift
+++ Tumblr/TabBarViewController.swift
@@ -12,9 +12,37 @@ class TabBarViewController: UIViewController {
 
     @IBOutlet weak var contentView: UIView!
     
+    var HomeViewController: UIViewController!
+    var SearchViewController: UIViewController!
+    var ComposeViewController: UIViewController!
+    var AccountViewController: UIViewController!
+    var TrendingViewController: UIViewController!
+    
+    var viewControllers: [UIViewController]!
+    
+    var selectedIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let storyboard  = UIStoryboard(name: "Main", bundle: nil)
+        
+        HomeViewController = storyboard.instantiateViewControllerWithIdentifier("HomeViewController")
+        
+        SearchViewController = storyboard.instantiateViewControllerWithIdentifier("SearchViewController")
+        
+        ComposeViewController = storyboard.instantiateViewControllerWithIdentifier("ComposeViewController")
+        
+        AccountViewController = storyboard.instantiateViewControllerWithIdentifier("AccountViewController")
+        
+        TrendingViewController = storyboard.instantiateViewControllerWithIdentifier("TrendingViewController")
+        
+        viewControllers = [HomeViewController, SearchViewController, ComposeViewController, AccountViewController, TrendingViewController]
 
+        
+        buttons[selectedIndex].selected = true
+        didPressTab(buttons[selectedIndex])
+        
         // Do any additional setup after loading the view.
     }
 
@@ -23,7 +51,26 @@ class TabBarViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBOutlet var buttons: [UIButton]!
+    
+    
+    @IBAction func didPressTab(sender: UIButton) {
+        let previousIndex = selectedIndex
+        selectedIndex = sender.tag
+        buttons[previousIndex].selected = false        
+        let previousVC = viewControllers[previousIndex]
+        previousVC.willMoveToParentViewController(nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParentViewController()
+        sender.selected = true
+        let vc = viewControllers[selectedIndex]
+        addChildViewController(vc)
+        vc.view.frame = contentView.bounds
+        contentView.addSubview(vc.view)
+        vc.didMoveToParentViewController(self)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
